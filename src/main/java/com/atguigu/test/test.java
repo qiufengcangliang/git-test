67d113e2be0ae87ff1260e91d43667fc98c6dc90
yang <2411028539@qq.com>
2022-11-06 15:31:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/test.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @author liuyang
+ * @create 2022-11-06 15:30
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
