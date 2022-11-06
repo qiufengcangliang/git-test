623dc09ad506351fd6d738b17894e9ed76b8f128
qiufengcangliang <101852501+qiufengcangliang@users.noreply.github.com>
2022-11-06 16:29:47 +0800

    pull commit

--- src/main/java/com/atguigu/test/test.java
+++ src/main/java/com/atguigu/test/test.java
@@ -14,5 +14,6 @@ public class test {
         System.out.println("master test");
         System.out.println("hello test5 -- hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
